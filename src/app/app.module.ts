5f90eebabb4eaefd2130ae2d4d3ddffac4108ba9
dimchik_st@list.ru <dimchik_st@list.ru>
2022-08-09 13:58:18 +0300

    Install ngrx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
+import { environment } from 'src/environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './core/components/app/app.component';
 import { CoreModule } from './core/core.module';
+import { reducers } from './app.state';
 
 @NgModule({
   imports: [
     BrowserModule,
     AppRoutingModule,
+    StoreModule.forRoot(reducers),
+    EffectsModule.forRoot([]),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: environment.production,
+      autoPause: true,
+    }),
     CoreModule,
   ],
   providers: [],
